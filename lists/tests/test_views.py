d4273fb9c0c58fe59ef419410188427cc7f05f21
João Paulo Bernhardt <joaopbernhardt@gmail.com>
2017-10-25 15:45:35 -0300

    Add item form to html and test_views

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -2,7 +2,7 @@ from django.test import TestCase
 from django.urls import resolve
 from django.http import HttpRequest
 from django.utils.html import escape
-
+from lists.forms import ItemForm
 from lists.views import home_page
 from lists.models import Item, List
 
@@ -13,6 +13,11 @@ class HomePageTest(TestCase):
     def test_home_page_returns_correct_html(self):
         response = self.client.get('/')        
         self.assertTemplateUsed(response, 'home.html')
+
+    def test_home_page_uses_item_form(self):
+        response = self.client.get('/')
+        self.assertIsInstance(response.context['form'], ItemForm)
+
 
 class ListViewTest(TestCase):
 
