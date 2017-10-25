a1ab1d4876120cec64484a8962f7c0e4ae7fb1d6
João Paulo Bernhardt <joaopbernhardt@gmail.com>
2017-10-25 17:56:24 -0300

    Use form in all views - FTs and UTs are back to normal

--- functional_tests/test_layout_and_styling.py
+++ functional_tests/test_layout_and_styling.py
@@ -6,7 +6,7 @@ class LayoutAndStyleTest(FunctionalTest):
         self.browser.get(self.live_server_url)
         self.browser.set_window_size(1024, 768)
 
-        inputbox = get_item_input_box()
+        inputbox = self.get_item_input_box()
         self.assertAlmostEqual(
             inputbox.location['x'] + inputbox.size['width']/2, 
             512,
@@ -16,7 +16,7 @@ class LayoutAndStyleTest(FunctionalTest):
         inputbox.send_keys('testing')
         inputbox.send_keys(Keys.ENTER)
         self.wait_for_row_in_list_table('1: testing')
-        inputbox = get_item_input_box()
+        inputbox = self.get_item_input_box()
         self.assertAlmostEqual(
             inputbox.location['x'] + inputbox.size['width']/2, 
             512,
--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -6,24 +6,31 @@ class ItemValidationTest(FunctionalTest):
     def test_cannot_add_empty_item(self):
 
         self.browser.get(self.live_server_url)
+
+        # Sends invalid data
         self.get_item_input_box().send_keys(Keys.ENTER)
+        self.wait_for(lambda: 
+            self.browser.find_element_by_css_selector('#id_text:invalid')
+            )
 
-        self.wait_for(lambda: self.assertEqual(
-            self.browser.find_element_by_css_selector('.has-error').text,
-            "You can't have an empty list item."
-            ))
-
+        # Fills valid and send
         self.get_item_input_box().send_keys('Buy Milk!')
+        self.wait_for(lambda:
+            self.browser.find_element_by_css_selector('#id_text:valid')
+            )
         self.get_item_input_box().send_keys(Keys.ENTER)
         self.wait_for_row_in_list_table('1: Buy Milk!')
 
+        # Another invalid entry
         self.get_item_input_box().send_keys(Keys.ENTER)
 
-        self.wait_for(lambda: self.assertEqual(
-            self.browser.find_el ement_by_css_selector('.has-error').text,
-            "You can't have an empty list item."
-            ))
-
+        # Fills second item
         self.get_item_input_box().send_keys('Make tea!')
+        self.wait_for(lambda:
+            self.browser.find_element_by_css_selector('#id_text:valid')
+            )
         self.get_item_input_box().send_keys(Keys.ENTER)
+
+        # Finnaly, both items are displayed
+        self.wait_for_row_in_list_table('1: Buy Milk!')
         self.wait_for_row_in_list_table('2: Make tea!')
