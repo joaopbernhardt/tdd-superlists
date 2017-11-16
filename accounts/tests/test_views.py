b1102c28a7bf535a30292c4ab511bd1e605d0a25
João Paulo Bernhardt <joaopbernhardt@gmail.com>
2017-11-16 19:04:20 -0300

    Added mock test for send_mail, UT for message display on success

--- accounts/tests/test_views.py
+++ accounts/tests/test_views.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 import accounts.views
+from unittest.mock import patch
 
 class SendLoginEmailViewTest(TestCase):
     def test_redirects_to_home_page(self):
@@ -8,23 +9,24 @@ class SendLoginEmailViewTest(TestCase):
             })
         self.assertRedirects(response, '/')
 
-    def test_sends_mail_to_address_from_POST(self):
-        self.send_mail_called = False
-
-        def fake_send_mail(subject, body, from_email, to_list):
-            self.send_mail_called = True
-            self.subject = subject
-            self.body = body
-            self.from_email = from_email
-            self.to_list = to_list
-
-        accounts.views.send_mail = fake_send_mail
+    @patch('accounts.views.send_mail')
+    def test_sends_mail_to_address_from_POST(self, mock_send_mail):
 
         self.client.post('/accounts/send_login_email', data={
             'email': 'jose@example.com'
             })
 
-        self.assertTrue(self.send_mail_called)
-        self.assertEqual(self.subject, 'Your login link to Superlists')
-        self.assertEqual(self.from_email, 'noreply@superlists')
-        self.assertEqual(self.to_list, ['jose@example.com'])
+        self.assertTrue(mock_send_mail.called)
+        (subject, body, from_email, to_list), kwargs = mock_send_mail.call_args
+        self.assertEqual(subject, 'Your login link to Superlists')
+        self.assertEqual(from_email, 'noreply@superlists')
+        self.assertEqual(to_list, ['jose@example.com'])
+
+    def test_adds_success_message(self):
+        response = self.client.post('/accounts/send_login_email', data={
+            'email' : 'jose@example.com'
+            }, follow=True)
+        message = list(response.context['messages'])[0]
+        self.assertEqual(message.message, 
+            'Check your email, we have sent you a log in link.')
+        self.assertEqual(message.tags, 'success')
