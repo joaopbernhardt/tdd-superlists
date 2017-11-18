821a808922183028adbe8e54264bcaaf00814b35
João Paulo Bernhardt <joaopbernhardt@gmail.com>
2017-11-17 23:53:14 -0300

    Tests for staging server now use real emails

--- functional_tests/test_login.py
+++ functional_tests/test_login.py
@@ -2,17 +2,25 @@ from django.core import mail
 from selenium.webdriver.common.keys import Keys
 import re
 
+import os
+import poplib
+import time
+
 from .base import FunctionalTest
 
-TEST_EMAIL = "edith@example.com"
 SUBJECT = 'Your login link for Superlists'
 
 class LoginTest(FunctionalTest):
 
     def test_can_get_email_link_to_log_in(self):
+        
+        if self.staging_server:
+            test_email = 'carlosdcosta19@gmail.com'
+        else:
+            test_email = 'jose@example.com'
         # Notices email field, fills it
         self.browser.get(self.live_server_url)
-        self.browser.find_element_by_name('email').send_keys(TEST_EMAIL)
+        self.browser.find_element_by_name('email').send_keys(test_email)
         self.browser.find_element_by_name('email').send_keys(Keys.ENTER)
 
         # Message tells her email has been sent
@@ -22,15 +30,13 @@ class LoginTest(FunctionalTest):
             ))
 
         # She checks and finds the message
-        email = mail.outbox[0]
-        self.assertIn(TEST_EMAIL, email.to)
-        self.assertEqual(email.subject, SUBJECT)
+        body = self.wait_for_email(test_email, SUBJECT)
 
         # Has a url in it
-        self.assertIn('Use this link to log in', email.body)
-        url_search = re.search(r'http://.+/.+$', email.body)
+        self.assertIn('Use this link to log in', body)
+        url_search = re.search(r'http://.+/.+$', body)
         if not url_search:
-            self.fail(f'Could not find url in email body:\n{email.body}')
+            self.fail(f'Could not find url in email body:\n{body}')
         url = url_search.group(0)
         self.assertIn(self.live_server_url, url)
 
@@ -38,9 +44,39 @@ class LoginTest(FunctionalTest):
         self.browser.get(url)
 
         # She is logged in
-        self.wait_to_be_logged_in(email=TEST_EMAIL)
+        self.wait_to_be_logged_in(email=test_email)
 
         # She logs out
         self.browser.find_element_by_link_text('Log out').click()
 
-        self.wait_to_be_logged_out(email=TEST_EMAIL)
+        self.wait_to_be_logged_out(email=test_email)
+
+    def wait_for_email(self, test_email, subject):
+        if not self.staging_server:
+            email = mail.outbox[0]
+            self.assertIn(test_email, email.to)
+            self.assertEqual(email.subject, subject)
+            return email.body
+
+        email_id = None
+        start = time.time()
+        inbox = poplib.POP3_SSL('pop.gmail.com')
+        try:
+            inbox.user(test_email)
+            inbox.pass_(os.environ['EMAIL_PASSWORD'])
+            while time.time() - start < 60:
+                count, _ = inbox.stat()
+                for i in reversed(range(max(1, count - 10), count + 1)):
+                    print('getting message', i)
+                    _, lines, __ = inbox.retr(i)
+                    lines = [l.decode('utf8') for l in lines]
+                    print(lines)
+                    if f'Subject: {subject}' in lines:
+                        email_id = i
+                        body = '\n'.join(lines)
+                        return body
+                time.sleep(5)
+        finally:
+            if email_id:
+                inbox.dele(email_id)
+            inbox.quit()
