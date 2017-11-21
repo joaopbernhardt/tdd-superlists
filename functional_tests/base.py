2dfe51b363022ecd47e8159e77fa238f9bb2c083
João Paulo Bernhardt <joaopbernhardt@gmail.com>
2017-11-21 12:47:22 -0300

    Moved create_session to base, created test_sharing FT

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -6,6 +6,9 @@ from .server_tools import reset_database
 import time
 import os
 from datetime import datetime
+from .server_tools import create_session_on_server
+from .management.commands.create_session import create_pre_authenticated_session
+from django.conf import settings
 
 SCREEN_DUMP_LOCATION = os.path.join(
     os.path.dirname(os.path.abspath(__file__)), 'screendumps'
@@ -99,3 +102,16 @@ class FunctionalTest(StaticLiveServerTestCase):
         self.get_item_input_box().send_keys(Keys.ENTER)
         item_number = num_rows + 1
         self.wait_for_row_in_list_table(f'{item_number}: {item_text}')
+
+    def create_pre_authenticated_session(self, email):
+        if self.staging_server:
+            session_key = create_session_on_server(self.staging_server, email)
+        else:
+            session_key = create_pre_authenticated_session(email)
+
+        self.browser.get(self.live_server_url + "/404_no_such_url/")
+        self.browser.add_cookie(dict(
+            name=settings.SESSION_COOKIE_NAME,
+            value=session_key,
+            path='/',
+            ))
